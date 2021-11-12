9b540d634cc8aed22772f212ba39ff5881817d7f
Makdoom Shaikh <makshaikh99@gmail.com>
2021-11-12 11:19:06 +0530

    done with sideicon

--- src/components/SideIcon/SideIcon.js
+++ src/components/SideIcon/SideIcon.js
@@ -1,8 +1,30 @@
 import React from "react";
 import "./sideIcon.css";
+import { IconButton } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
 
 const SideIcon = () => {
-  return <div className="sideicon__container">Sideicon</div>;
+  return (
+    <div className="sideicon__container">
+      <IconButton className="icon-btn">
+        <img src="/images/calender.png" alt="" />
+      </IconButton>
+      <IconButton className="icon-btn">
+        <img src="/images/keep.png" alt="" />
+      </IconButton>
+      <IconButton className="icon-btn">
+        <img src="/images/task.png" alt="" />
+      </IconButton>
+      <IconButton className="icon-btn">
+        <img src="/images/contact.png" alt="" />
+      </IconButton>
+      <div>
+        <IconButton className="icon-btn add">
+          <AddIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
 };
 
 export default SideIcon;
